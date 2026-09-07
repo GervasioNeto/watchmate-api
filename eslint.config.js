967a668f2e230fd691c6baefc8ec3a8abeaaa1ad
Gervásio Neto <gervasiolopesn@gmail.com>
2026-09-07 18:50:58 -0300

    feat: bootstrap project skeleton with Express and TypeScript

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+const tseslint = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
+const prettierConfig = require('eslint-config-prettier');
+
+module.exports = [
+  {
+    ignores: ['dist/', 'node_modules/'],
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 2022,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+    },
+  },
+  prettierConfig,
+];
